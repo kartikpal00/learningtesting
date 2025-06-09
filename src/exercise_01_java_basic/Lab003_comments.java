d2d2ab2a07b2d3c78d8ba731c0980e52fe5fccb5
kartikpal22 <kartikpal500@gmail.com>
2025-06-09 22:08:49 +0530

    Lab 003 comments

---
+++ src/exercise_01_java_basic/Lab003_comments.java
@@ -0,0 +1,13 @@
+package exercise_01_java_basic;
+
+public class Lab003_comments {
+    public static void main(String[] args) {
+  System.out.println("comments");
+    //this is a comments
+        /*System.out.println("hye");
+        System.out.println("bye");
+        System.out.println("drink");*/
+//        this is a multiline comments
+    }
+
+}
