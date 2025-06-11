f8971580bfba19811e5296503e0abef71e258cce
kartikpal22 <kartikpal500@gmail.com>
2025-06-11 08:14:43 +0530

    keyword and identifier

---
+++ src/exercise_01_java_basic/Lab006_good_comments.java
@@ -0,0 +1,18 @@
+package exercise_01_java_basic;
+
+public class Lab006_good_comments {
+    /*Author - pramod
+    course - ATB13X
+    this is a documentation comments
+            comments in arguments*/
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
+
+/*
+keyword is a reserve word start is is smaall word
+identifier is is a identify word for example exersice_01_java_basic
+and Laboo6_good_comments is a identifier
+keyword is defined 53 keywod in java */
