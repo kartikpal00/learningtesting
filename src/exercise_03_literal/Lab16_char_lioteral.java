b9db83aac9133536bffa057ca420905f5cbda6b9
kartikpal22 <kartikpal500@gmail.com>
2025-06-12 20:44:34 +0530

    literal charactor

---
+++ src/exercise_03_literal/Lab16_char_lioteral.java
@@ -0,0 +1,24 @@
+package exercise_03_literal;
+
+public class Lab16_char_lioteral {
+    public static void main(String[] args) {
+        char c1 = 'A'; // A to Z ,a to z , !@#$5^&*()_-+=;
+        char c2 = 'B';
+        char c3 = '@';
+        char c4 = '-';
+        char c5 = '9';
+        char c6 = '1';
+
+        //Escape Char
+        char new_line = '\n';
+        char tab_line = '\t';
+        char back_space = '\b';
+        char carriage_return = '\r';
+
+     System.out.println("kartikpal");
+        System.out.println("kartik"+new_line+"pal");
+        System.out.println("kartik"+tab_line+"pal");
+        System.out.println("kartik"+back_space+"pal");
+        System.out.println("kartik"+carriage_return+"pal");
+    }
+}
