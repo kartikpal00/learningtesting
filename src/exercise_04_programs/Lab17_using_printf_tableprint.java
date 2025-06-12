05dbb5a20faaa559b78a6717f625ac3dbeb52b48
kartikpal22 <kartikpal500@gmail.com>
2025-06-12 20:43:14 +0530

    using printf table print

---
+++ src/exercise_04_programs/Lab17_using_printf_tableprint.java
@@ -0,0 +1,48 @@
+package exercise_04_programs;
+
+public class Lab17_using_printf_tableprint {
+    public static void main(String[] args) {
+        int a= 9;
+        int b= 1;
+        System.out.printf("%dx%d=%d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%dx%d=%d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%dx%d=%d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%dx%d=%d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%dx%d=%d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%dx%d=%d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%dx%d=%d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%dx%d=%d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%dx%d=%d",a,b,a*b);
+        System.out.println();
+        b++;
+
+        System.out.printf("%dx%d=%d",a,b,a*b);
+        System.out.println();
+        b++;
+
+    }
+}
