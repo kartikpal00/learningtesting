28e1edc35475c21913ad219aeb23447ebefd90df
kartikpal22 <kartikpal500@gmail.com>
2025-06-13 08:41:35 +0530

    program table

---
+++ src/exercise_04_programs/Lab19_program_table.java
@@ -0,0 +1,26 @@
+package exercise_04_programs;
+
+public class Lab19_program_table {
+    public static void main(String[] args) {
+        int num=7;
+        System.out.printf("%dx%d = %d", num, 1, num);
+        System.out.println();
+        System.out.printf("%dx%d = %d", num, 2,num*2);
+        System.out.println("");
+        System.out.printf("%dx%d = %d", num, 3,num*3);
+        System.out.println("");
+        System.out.printf("%dx%d = %d", num, 4,num*4);
+        System.out.println("");
+        System.out.printf("%dx%d = %d", num, 5,num*5);
+        System.out.println("");
+        System.out.printf("%dx%d = %d", num, 6,num*6);
+        System.out.println("");
+        System.out.printf("%dx%d = %d", num, 7,num*7);
+        System.out.println("");
+        System.out.printf("%dx%d = %d", num, 8,num*8);
+        System.out.println("");
+        System.out.printf("%dx%d = %d", num, 9,num*9);
+        System.out.println("");
+        System.out.printf("%dx%d = %d", num, 10,num*10);
+    }
+    }
