7e78acb8e3e0e9f9c5b3fadd5bd4bf6c72e80b98
kartikpal22 <kartikpal500@gmail.com>
2025-06-13 08:14:50 +0530

    exercise

---
+++ src/exercise_03_literal/Lab17_non_primitive_literal.java
@@ -0,0 +1,18 @@
+package exercise_03_literal;
+
+public class Lab17_non_primitive_literal {
+    public static void main(String[] args) {
+//        primitive data type - DEFINED by java guys
+//                max , min , size defined
+        byte b = 10; //1 byte -> 8 bits
+        int age = 65; // 4 byte -> 32 bits
+        //Non primitve data type (defined by ujser ) , reference data type
+        //No sizer , max , min
+        String name = "kartik"; //string is bunmch of char;
+        int[] arrays_of_items = new int [10];
+        //string , arrays. classes ,enum
+
+        String s = null;
+        //int age = null'
+    }
+}
