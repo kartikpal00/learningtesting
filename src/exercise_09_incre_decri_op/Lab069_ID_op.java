2139496e1931b878a6afb85f2f5175f9cbd31072
kartikpal22 <kartikpal500@gmail.com>
2025-06-16 20:42:47 +0530

    increment decrement

---
+++ src/exercise_09_incre_decri_op/Lab069_ID_op.java
@@ -0,0 +1,8 @@
+package exercise_09_incre_decri_op;
+
+public class Lab069_ID_op {
+    public static void main(String[] args) {
+        String age_string = args[0];
+        System.out.println(age_string);
+    }
+}
