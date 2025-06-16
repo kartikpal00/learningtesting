744f5c34de14a01f77c4821d629eebd4aa715713
kartikpal22 <kartikpal500@gmail.com>
2025-06-16 20:44:02 +0530

    increment decrement

---
+++ src/exercise_08_userinput/Lab_Userinput_CLI.java
@@ -0,0 +1,9 @@
+package exercise_08_userinput;
+
+public class Lab_Userinput_CLI {
+    public static void main(String[] args) {
+        int age = 65;
+        String canIvote = age>18 ? "yes" : "no";
+        System.out.println(age);
+    }
+}
